c84adcce7b275f6550bc77abff16376e1fc2c1c0
DmytryGal <36880734+DmytryGal@users.noreply.github.com>
2021-09-29 23:04:29 +0400

    Исправиления на замечания

--- js/main.js
+++ js/main.js
@@ -1,22 +1,17 @@
 /* Функция, возвращающая случайное целое число из переданного диапазона включительно*/
-const getRandomInteger = (min,max) => {
-  if (min < max && min >= 0) {
-    return Math.floor(Math.random()*(max - min + 1) + min);
-  }
-  else{
-    throw new Error ('Значение min не должно быть меньше нуля');
-  }
+const getRandomInteger = (min, max) => {
+    if (min < max && min >= 0) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+    else {
+        throw new Error('Значение min не должно быть меньше нуля');
+    }
 };
-getRandomInteger(0,10);
+getRandomInteger(0, 10);
 /*https://html5css.ru/js/js_random.php*/
 
 /*Функция для проверки максимальной длины строки*/
-const checkMaxStringleLength = (value,maxString) =>{
-  if(value.length <= maxString ){
-    return true;
-  }
-  else {
-    return false;
-  }
+const checkMaxStringleLength = (value, maxLength) => {
+    return value.length <= maxLenght
 };
-checkMaxStringleLength('',5);
+checkMaxStringleLength('', 5);
