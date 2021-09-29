b9371aeca9fce7e4faaf2e6399724f13bf5d4951
DmytryGal <36880734+DmytryGal@users.noreply.github.com>
2021-09-29 22:29:07 +0400

    Изменил предыдущий комит

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
-//Фунция Функция, возвращающая случайное целое число из переданного диапазона включительно
-const getRandomNumber = (min,max) => {
-    if(min<max){return Math.floor(Math.random()*(max - min + 1) + min);} 
-  else{return Math.floor(Math.random()*(min - max + 1) + max);}};
-getRandomNumber(0,10);
-//https://html5css.ru/js/js_random.php
-
-//Функция для проверки максимальной длины строки
-const getCompareStrings = (myString,resonString) =>{
-  if(myString.length===resonString || myString.length<resonString){return true;}
-  else {return false;}
+/* Функция, возвращающая случайное целое число из переданного диапазона включительно*/
+const getRandomInteger = (min,max) => {
+  if (min < max && min >= 0) {
+    return Math.floor(Math.random()*(max - min + 1) + min);
+  }
+  else{
+    throw new Error ('Значение min не должно быть меньше нуля');
+  }
 };
-getCompareStrings('',5);
+getRandomInteger(0,10);
+/*https://html5css.ru/js/js_random.php*/
+
+/*Функция для проверки максимальной длины строки*/
+const checkMaxStringleLength = (value,maxString) =>{
+  if(value.length <= maxString ){
+    return true;
+  }
+  else {
+    return false;
+  }
+};
+checkMaxStringleLength('',5);
