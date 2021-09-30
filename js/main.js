f0f4c2ea74b647ea3fc321c9a2cc51810b610575
DmytryGal <36880734+DmytryGal@users.noreply.github.com>
2021-10-01 00:05:12 +0400

    Добавил условия для вывода случайных чисел

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
 /* Функция, возвращающая случайное целое число из переданного диапазона включительно*/
+
 const getRandomInteger = (min, max) => {
-  if (min < max && min >= 0) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+  if (min < 0) {
+    throw new Error('value min cannot be less than zero');
   }
-  else {
-    throw new Error('Значение min не должно быть меньше нуля');
+  if (max < 0) {
+    throw new Error('value max cannot be less than zero');
   }
+  if (max <= min) {
+    throw new Error('value max cannot be less or equal than min');
+  }
+  return Math.floor(Math.random() * (max - min + 1) + min);
+
 };
-getRandomInteger(0, 10);
+getRandomInteger(0, 15);
+
 /*https://html5css.ru/js/js_random.php*/
 
 /*Функция для проверки максимальной длины строки*/
