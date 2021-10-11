9053da926b2abbdf4d33ab395192b8a74370f1e6
DmytryGal <36880734+DmytryGal@users.noreply.github.com>
2021-10-11 20:50:05 +0300

    Отработал замечания

--- js/main.js
+++ js/main.js
@@ -1,59 +1,64 @@
-//Фунция Функция, возвращающая случайное целое число из переданного диапазона включительно
+const PHOTOS_COUNT = 25;
+const DESCRIPTION = ['как-то так', 'ни то ни се', 'не понятно'];
+const MESSAGE = ['Всё отлично!', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают.', 'Как можно было поймать такой неудачный момент?!'];
+const USER_NAME = ['Иван', 'Лина', 'Потап', 'Ирэн', 'Михон', 'Кузмич', 'Димон'];
+
 const getRandomNumber = (min, max) => {
-  if (min < max) { return Math.floor(Math.random() * (max - min + 1) + min); }
-  else { return Math.floor(Math.random() * (min - max + 1) + max); }
+  if (min < 0) {
+    throw new Error('value min cannot be less than zero');
+  }
+  if (max < 0) {
+    throw new Error('value max cannot be less than zero');
+  }
+  if (max <= min) {
+    throw new Error('value max cannot be less or equal than min');
+  }
+  return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-//https://html5css.ru/js/js_random.php
+function addZero(num) {
+  if (num < 10) {
+    num = `0${num}`;
+  }
+  return num;
+}
+const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
 
-//Функция для проверки максимальной длины строки
-const getCompareStrings = (myString, resonString) => {
-  if (myString.length === resonString || myString.length < resonString) { return true; }
-  else { return false; }
-};
-
-
-const DESCRIPTION = ['как-то так', 'ни то ни се', 'не понятно',];
-const MESSAGE = ['Всё отлично!', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают.', 'Как можно было поймать такой неудачный момент?!']
-
-
-const USER_NAME = ['Иван', 'Лина', 'Потап', 'Ирэн', 'Михон', 'Кузмич', 'Димон']
-
-const fotoNumb = getRandomNumber(1, 25);
-const avaNumb = getRandomNumber(1, 6);
-
-const getRandomArrayElement = (elements) => {
-  return elements[getRandomNumber(0, elements.length - 1)]
-};
-const generatePhoto = (id) => {
+function generateComment(id) {
+  const avaNumb = getRandomNumber(1, 6);
   return {
-    id: '',
-    url: 'photos/fotoNumb.jpg,',
-    description: getRandomArrayElement(DESCRIPTION),
-    likes: getRandomNumber(15, 250),
-    comments: generateComment,
-  };
-};
-const generatePhotos = () => {
-  const photos = getRandomNumber(1, 25);
-  for (let i = 1; i <= photos; i++) {
-    photos.push(generatePhoto(i));
-  };
-};
-
-const generateComment = (id) => {
-  return {
-    id: '',
-    avatar: 'img/avatar-avaNumb.svg',
+    id,
+    avatar: `img/avatar-${addZero(avaNumb)}.svg`,
     message: getRandomArrayElement(MESSAGE),
     name: getRandomArrayElement(USER_NAME),
   };
-};
+}
+
 const generateComments = () => {
-  const comments = getRandomNumber(0, 25);
-  for (let i = 1; i <= comments; i++) {
-    comments.push(generateComment(i));
+  const comments = [];
+  for (let index = 1; index <= PHOTOS_COUNT; index++) {
+    comments.push(generateComment(index));
   }
   return comments;
 };
-const publicPhoto = generateComment;
+const generatePhoto = (id) => {
+  const fotoNumb = getRandomNumber(1, 25);
+  return {
+    id,
+    url: `photos/${addZero(fotoNumb)}.jpg,`,
+    description: getRandomArrayElement(DESCRIPTION),
+    likes: getRandomNumber(15, 250),
+    comments: generateComments(),
+  };
+};
+const generatePhotos = () => {
+  const photos = [];
+  for (let index = 1; index <= PHOTOS_COUNT; index++) {
+    photos.push(generatePhoto(index));
+    return photos;
+  }
+};
+const publicPhotos = generatePhotos();
+
+// eslint-disable-next-line no-console
+console.log(publicPhotos);
