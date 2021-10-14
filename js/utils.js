09d2c59d4c15b182026da1ebf8f1a8f70385b110
DmytryGal <36880734+DmytryGal@users.noreply.github.com>
2021-10-14 19:44:32 +0300

    Сохранл модули

---
+++ js/utils.js
@@ -0,0 +1,31 @@
+const getRandomNumber = (min, max) => {
+  if (min < 0) {
+    throw new Error('value min cannot be less than zero');
+  }
+  if (max < 0) {
+    throw new Error('value max cannot be less than zero');
+  }
+  if (max <= min) {
+    throw new Error('value max cannot be less or equal than min');
+  }
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const addZero = (num) => {
+  if (num < 10) {
+    num = `0${num}`;
+  }
+  return num;
+};
+//Функция для проверки максимальной длины строки
+const getCompareStrings = (myString, resonString) => {
+  if (myString.length === resonString || myString.length < resonString) {
+    return true;
+  }
+  else {
+    return false;
+  }
+};
+// eslint-disable-next-line no-console
+console.log(getCompareStrings);
+export{getRandomNumber, addZero};
